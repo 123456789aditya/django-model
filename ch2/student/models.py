4aa1a2db45af8def1dab0f1ed8f4094e48774c89
aditya pathak <pathakaditya881@gmail.com>
2025-04-18 23:25:52 +0530

    added

---
+++ ch2/student/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class profile(models.Model):
+    name=models.CharField(max_length=70)
+    email=models.EmailField(max_length=240)
+    city=models.CharField(max_length=100)
+    roll=models.IntegerField(max_length=70)
+    state=models.CharField(max_length=60)
