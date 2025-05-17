3a6f490e213687956e45e077b923b4c9fc57db86
aditya pathak <pathakaditya881@gmail.com>
2025-05-17 22:31:25 +0530

    added

--- ch9-post-req/student/views.py
+++ ch9-post-req/student/views.py
@@ -1,10 +1,10 @@
-# from django.shortcuts import render
-# from student.forms import Registration
+from django.shortcuts import render
+from student.forms import Registration
 
-# # Create your views here.
-# def register(request):
-#     if request.method=='POST':
-#         regr=Registration(request.POST)
-#         print(regr)
-#     reg=Registration()
-#     return render(request,'student/registration.html',{'regr':reg})
+# Create your views here.
+def register(request):
+    if request.method=='POST':
+        regr=Registration(request.POST)
+        print(regr)
+    reg=Registration()
+    return render(request,'student/registration.html',{'regr':reg})
